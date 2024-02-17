473c63387bfbb64b484f4f4c025c4a5065eba5ca
Caleb Hatch <hatchcaleb@gmail.com>
2024-02-17 12:01:30 -0600

    added title, dropdown on products

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "./Search";
-import cart from '../icons/cart.png'
+import cart from "../icons/cart.png";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
   const onProducts = (event) => {
@@ -18,10 +19,25 @@ const Header = () => {
 
   return (
     <header className="flex justify-between items-center border-b-2 bg-main h-14 z-10">
-      <h1>Title</h1>
+      <h1>FusionStyle</h1>
       <container className="flex justify-center gap-6">
         <button onClick={onHome}>Home</button>
-        <button onClick={onProducts}>Products</button>
+        <div
+          className="relative"
+          onMouseEnter={() => setIsOpen(true)}
+          onMouseLeave={() => setIsOpen(false)}
+        >
+          <button onClick={onProducts} className="focus:outline-none focus:shadow-outline">
+            Products
+          </button>
+          {isOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg z-10">
+              <p>Option 1</p>
+              <p>Option 2</p>
+              <p>Option 3</p>
+            </div>
+          )}
+        </div>
         <button onClick={onDeals}>Monthly Deals</button>
         <SearchBar />
       </container>
